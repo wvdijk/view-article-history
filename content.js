0fa196b8c187e336f784260d2c37343d65d32705
Wieland van Dijk <wvdijk@gmail.com>
2017-09-06 10:34:02 +0200

    Update content.js
    Moved stuff to functions. Added confirmation dialog before Honk import.

--- content.js
+++ content.js
@@ -1,32 +1,43 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      (function(){
+    function(request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
+            (function() {
+                var url = window.location + '/history';
+                var req = new XMLHttpRequest();
+                req.open('GET', url, false);
+                req.send(null);
+                if (req.status == 200) {
+                    if (req.responseText.indexOf('wordpress_honk') > -1) {
+                        openHonk();
+                    } else {
+                        var check = confirm('Dit artikel is niet eerder geopend in Honk. Bewerk het in GN4, tenzij dat echt niet anders kan.\n\nKlik op OK om deze dialoog te sluiten.\n\nWeet je zeker dat je toch in Honk verder wil werken? Klik dan op Cancel/Annuleren.');
+                        if (check) {
+                            return;
+                        } else {
+                            openHonk();
+                        }
+                    }
+                }
+            })();
 
-		var url = window.location + '/history';
-		var req = new XMLHttpRequest();
-		req.open('GET', url, false);
-		req.send(null);
-		if(req.status == 200) {
-			if (req.responseText.indexOf('wordpress_honk') > -1) {
-				// onderstaande code van de Edit in Honk bookmarklet
-        var articleId = document.body.getAttribute('data-article-id');
-        if (!articleId) {
-          var m = location.pathname.match(/\-a(\d+)/);
-          if (m) {
+        }
+    });
+
+
+function getEditUrl() {
+    var articleId = document.body.getAttribute('data-article-id');
+    if (!articleId) {
+        var m = location.pathname.match(/\-a(\+d)/);
+        if (m) {
             articleId = m[1];
-          }
+        } else {
+            console.log('Geen NRC-artikel-ID gevonden')
         }
-        var editUrl = 'https://honk.nrc.nl/bvhw/?nrc_action=hub2import&article_id=' + articleId + '&action=edit';
-        if (editUrl) {
-          window.open(editUrl, '_blank', null);
-        }
-			}
-		else {
-				alert('Dit artikel is nog niet geopend in Honk, bewerk het in GN4 tenzij het echt niet anders kan.');
-			}
-		}
-	})();
- }
+    }
+    return 'https://honk.nrc.nl/bvhw/?nrc_action=hub2import&article_id=' + articleId + '&action=edit';
 }
-);
+
+function openHonk() {
+    var l = getEditUrl();
+    window.open(l, '_blank', null);
+}
